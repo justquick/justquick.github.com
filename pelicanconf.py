0b786847f73f3460609e055d0156fbcb0c8465a3
Justin Quick <justquick@gmail.com>
2016-07-18 21:44:27 -0400

    pelican quickstart

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Justin Quick'
+SITENAME = u'Justin Quick'
+SITEURL = ''
+#SITEURL = 'justquick.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Python', 'http://python.org/'),
+         ('Django', 'https://www.djangoproject.com'),
+        )
+# Social widget
+SOCIAL = (('Github', 'https://github.com/justquick'),
+          ('LinkedIn', 'https://www.linkedin.com/in/justin-quick-59997312'),
+          ('Facebook', 'https://www.facebook.com/justquick'),
+          ('Twitter', 'https://twitter.com/justquick'),
+          ('Google+', 'https://plus.google.com/+JustinQuick-justquick'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
